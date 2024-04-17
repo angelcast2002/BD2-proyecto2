a741fd32f2357ee3330c775f28d5a65a2329adc1
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2024-04-16 19:39:36 -0600

    update

--- GulaGuide/src/pages/DishInfo/DishInfo.jsx
+++ GulaGuide/src/pages/DishInfo/DishInfo.jsx
@@ -5,10 +5,8 @@ import style from "./DishInfo.module.css";
 import useApi from "../../Hooks/useApi";
 
 
+const DishInfo = () => {
 
-const DishInfo = (name_dish) => {
-
-  name_dish = "Pollo Cocido"
 
   const api = useApi()
   const [dish_info, setDishInfo] = useState(null)
@@ -17,7 +15,7 @@ const DishInfo = (name_dish) => {
     let response
     response = await api.handleRequest(
       "GET",
-      "get/dish?dish_id=" + name_dish
+      "get/dish?dish_id=" + "Pollo Cocido"
     )
 
     const data = await response
@@ -41,32 +39,31 @@ const DishInfo = (name_dish) => {
   return (
     <div className={style.mainContainer}>
       <div className={style.dishContainer}>
-        <div className={style.dishContainerText}>
-          <h1>{dish_info.name}</h1>
-          <p>
-            <strong>{"Descripción: "}</strong> {" "}
-            {dish_info.description}
-          </p>
-          <p>
-            <strong>{"Precio promedio: "}</strong> {" "}
-            {"Q." + dish_info.avg_price + ".00"}
-          </p>
-          <p>
-            <strong>{"Vegano: "}</strong> {" "}
-            {dish_info.is_vegan ? "Sí" : "No"}
-          </p>
-          <p>
-            <strong>{"Contiene alcohol: "}</strong> {" "}
-            {dish_info.has_alcohol ? "Sí" : "No"}
-          </p>
-        </div>
+        {dish_info && (
+          <div className={style.dishContainerText}>
+            <h1>{dish_info.name}</h1>
+            <p>
+              <strong>{"Descripción: "}</strong> {" "}
+              {dish_info.description}
+            </p>
+            <p>
+              <strong>{"Precio promedio: "}</strong> {" "}
+              {"Q." + dish_info.avg_price + ".00"}
+            </p>
+            <p>
+              <strong>{"Vegano: "}</strong> {" "}
+              {dish_info.is_vegan ? "Sí" : "No"}
+            </p>
+            <p>
+              <strong>{"Contiene alcohol: "}</strong> {" "}
+              {dish_info.has_alcohol ? "Sí" : "No"}
+            </p>
+          </div>
+        )}
+      </div>style.
+      <div className={style.ingredientsContainer}>
+        <h1>Ingredientes</h1>
 
-      </div>
-      <div className={style.ingredientsContainer} >
-        <div className={style.ingredientsText}>
-          <h1>Ingredientes</h1>
-
-        </div>
       </div>
     </div>
   );
