8a0203da335a59fbb411130e770e92327a600949
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2024-04-17 02:12:07 -0600

    update

--- GulaGuide/src/pages/ResInfo/ResInfo.jsx
+++ GulaGuide/src/pages/ResInfo/ResInfo.jsx
@@ -6,10 +6,11 @@ import { useStoreon } from "storeon/react"
 import useApi from "../../Hooks/useApi"
 import CommentsComponent from "../../components/CommentsComponent/CommentsComponent"
 import MenuItem from "../../components/MenuItem/MenuItem"
+import Loader from "../../components/Loader/Loader"
+import Header from "../../components/Header/Header"
 
-const ResInfo = ({id}) => {
+const ResInfo = ({ id }) => {
   const restaurant_id = id
-  console.log("restaurant_id", id)
 
   const api = useApi()
   const { user } = useStoreon("user")
@@ -20,14 +21,22 @@ const ResInfo = ({id}) => {
   const [location, setLocation] = useState("")
   const [comments, setComments] = useState(null)
 
+  const formatData = (data) => {
+    let horario = data.schedule.split("-")
+    setHorario(horario[0] + "hrs - " + horario[1] + "hrs")
+
+    let precios = data.prices.split("-")
+    setPrecios("Q." + precios[0] + ".00 - Q." + precios[1] + ".00")
+  }
   // datos del restaurante que se mostrarán en la página
   const gettingUserInfo = async () => {
     let response_user_info
     response_user_info = await api.handleRequest(
       "GET",
-      "get/restaurant?restaurant_id=" + restaurant_id // esto en realidad no está bien. 
+      "get/restaurant?restaurant_id=" + restaurant_id // esto en realidad no está bien.
     )
     const data = await response_user_info // Recibidos
+    formatData(data[0])
     setResInfo(data[0])
   }
 
@@ -63,20 +72,9 @@ const ResInfo = ({id}) => {
 
   useEffect(() => {
     gettingUserInfo()
-    if (resInfo) {
-      console.log("resInfo.schedule", resInfo.schedule)
-      let horario = resInfo.schedule.split("-")
-      setHorario(horario[0] + "hrs - " + horario[1] + "hrs")
-
-      console.log("resInfo.prices", resInfo.prices)
-      let precios = resInfo.prices.split("-")
-      setPrecios("Q." + precios[0] + ".00 - Q." + precios[1] + ".00")
-    }
-
     gettingMenu()
     getLocation()
     getComments()
-
   }, [])
 
   const handleAddVisit = async () => {
@@ -89,71 +87,80 @@ const ResInfo = ({id}) => {
     console.log("Presionando añadir a favoritos")
   }
 
-
-
-  console.log("menu: \n", menu)
-  console.log("resInfo", resInfo)
-  console.log("location", location)
-  console.log("comments", comments)
-
   return (
-    <div className={style.resInfoContainer}>
-      <div className={style.leftContainer}>
-        <div className={style.imgContainer}>
-          <img
-            src={
-              resInfo
-                ? resInfo.imagen
-                : "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg"
-            }
-            alt="Imagen del restaurante"
-          />
-        </div>
-        <h1>{resInfo ? resInfo.name : "Cargando Nombre"}</h1>
-        <div className={style.infoContainer}>
-          <div className={style.textContainer}>
-            <p className={style.address}>
-              <strong>Dirección:</strong>{" "}
-              {location ? (location.street + ", " + location.number + ", " + location.community) : "Cargando Dirección"}
-            </p>
-            <p className={style.rating}>
-              <strong>Rating:</strong>{" "}
-              {resInfo ? resInfo.rating : "Cargando Rating"}
-            </p>
-            <p>
-              <strong>Horario:</strong>{" "}
-              {resInfo ? horario : "Cargando Horario"}
-            </p>
-            <p>
-              <strong>Precios:</strong>{" "}
-              {resInfo ? precios : "Cargando Precios"}
-            </p>
-            {resInfo && resInfo.petFriendly && <p> Acepta mascotas </p>}
-            {resInfo && resInfo.sells_alcohol && <p> Se puede consumir alcohol </p>}
+    <div className={style.mainContainer}>
+      <Header />
+      <div className={style.resInfoContainer}>
+        {" "}
+        <div className={style.leftContainer}>
+          <div className={style.imgContainer}>
+            <img
+              src={
+                resInfo
+                  ? resInfo.imagen
+                  : "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg"
+              }
+              alt="Imagen del restaurante"
+            />
+          </div>
+          <h1>{resInfo ? resInfo.name : "Cargando Nombre"}</h1>
+          <div className={style.infoContainer}>
+            <div className={style.textContainer}>
+              <p className={style.address}>
+                <strong>Dirección:</strong>{" "}
+                {location
+                  ? location.street +
+                    ", " +
+                    location.number +
+                    ", " +
+                    location.community
+                  : "Cargando Dirección"}
+              </p>
+              <p className={style.rating}>
+                <strong>Rating:</strong>{" "}
+                {resInfo ? resInfo.rating : "Cargando Rating"}
+              </p>
+              <p>
+                <strong>Horario:</strong>{" "}
+                {resInfo ? horario : "Cargando Horario"}
+              </p>
+              <p>
+                <strong>Precios:</strong>{" "}
+                {resInfo ? precios : "Cargando Precios"}
+              </p>
+              {resInfo && resInfo.petFriendly && <p> Acepta mascotas </p>}
+              {resInfo && resInfo.sells_alcohol && (
+                <p> Se puede consumir alcohol </p>
+              )}
+            </div>
+          </div>
+          <CommentsComponent comments={comments} />
+          <div></div>
+          <div className={style.buttonContainer}>
+            <button className={style.visit} onClick={handleAddVisit}>
+              Añadir visita
+            </button>
+            <button className={style.comment} onClick={handleAddFavorite}>
+              Añadir a favoritos
+            </button>
           </div>
         </div>
-        <CommentsComponent comments={comments}/>
-        <div></div>
-        <div className={style.buttonContainer}>
-          <button className={style.visit} onClick={handleAddVisit}>Añadir visita</button>
-          <button className={style.comment} onClick={handleAddFavorite}>Añadir a favoritos</button>
-        </div>
-      </div>
-      <div className={style.rightContainer}>
-        <h2>Menú</h2>
-        <div className={style.menuContainer}>
-          {Array.isArray(menu) && menu.map((item, index) => {
-            console.log("AQUI AA", item.avg_price);
-            return (
-              <MenuItem
-                key={index}
-                name={item.name}
-                description={item.description}
-                avg_price={"Q." + item.avg_price + ".00"}
-              />
-            );
-          })}
-        </div>
+        <div className={style.rightContainer}>
+          <h2>Menú</h2>
+          <div className={style.menuContainer}>
+            {Array.isArray(menu) &&
+              menu.map((item, index) => {
+                return (
+                  <MenuItem
+                    key={index}
+                    name={item.name}
+                    description={item.description}
+                    avg_price={"Q." + item.avg_price + ".00"}
+                  />
+                )
+              })}
+          </div>
+        </div>{" "}
       </div>
     </div>
   )
