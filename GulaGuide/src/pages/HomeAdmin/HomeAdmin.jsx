421f1232e74e037fa99f3dc07c4e75aee7d6f11d
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2024-04-17 18:06:20 -0600

    Update HomeAdmin.jsx

--- GulaGuide/src/pages/HomeAdmin/HomeAdmin.jsx
+++ GulaGuide/src/pages/HomeAdmin/HomeAdmin.jsx
@@ -5,14 +5,29 @@ import Button from "../../components/Button/Button"
 import useApi from "../../Hooks/useApi"
 
 const HomeAdmin = () => {
-  const handleSend = () => {}
+  const handleSend = () => {} // Ya no se usa. 
   const handleTextArea = (e) => {
     setTextArea(e.target.value)
+    console.log(e.target.value)
   }
   const handleLogOut = () => {}
 
   const [textArea, setTextArea] = useState("")
   const [response, setResponse] = useState("")
+
+  const api = useApi()
+
+  
+  const sendQuery = async () => {
+    const res = await api.handleRequest(
+      "GET", 
+      "admin/execute_query?query=" + textArea
+      )
+      const data = await res
+      const plainTextData = JSON.stringify(data.data, null, 2) // Convertir a texto plano
+      console.log("data", data)
+      setResponse(plainTextData)
+  }
 
   return (
     <div className={styles.container}>
@@ -34,17 +49,16 @@ const HomeAdmin = () => {
           <TextAreaAuto
             name="textArea"
             type="text"
-            placeholder="Escribe aquí tu consulta..."
             onChange={handleTextArea}
             minRows={5}
-            maxRows={10}
+            maxRows={20}
             value={response}
             disabled
           />
         </div>
       </div>
       <div className={styles.buttonsContainer}>
-        <Button label="Enviar Consulta" onChange={handleSend} />
+        <Button label="Enviar Consulta" onClick={sendQuery} />
         <Button label="Salir" onChange={handleLogOut} />
       </div>
     </div>
